6d2440cae89ca8efcce33f47e41b7161a18dcdcf
Markus Heer <mail@markus-heer.com>
2020-04-08 14:26:14 +0200

    styling fixes

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -7,7 +7,7 @@ const Start: React.FC = () => {
     <Layout>
       <div className="flex flex-wrap justify-center">
         <div className="flex flex-wrap w-full lg:w-1/2">
-          <div className="flex w-1/2 h-64 p-2">
+          <div className="flex w-1/2 h-40 p-2">
             <Link
               to="/page/cumulated"
               className="flex justify-center items-center bg-turquois text-white text-center hover:bg-light-turquois w-full h-full font-roboto-mono text-base uppercase tracking-widest-2"
@@ -15,7 +15,7 @@ const Start: React.FC = () => {
               Cumulated Values
             </Link>
           </div>
-          <div className="flex w-1/2 h-64 p-2">
+          <div className="flex w-1/2 h-40 p-2">
             <Link
               to="/page/daily-rates"
               className="flex justify-center items-center bg-turquois text-white text-center hover:bg-light-turquois w-full h-full font-roboto-mono text-base uppercase tracking-widest-2"
@@ -23,7 +23,7 @@ const Start: React.FC = () => {
               Daily Rates
             </Link>
           </div>
-          <div className="flex w-1/2 h-64 p-2">
+          <div className="flex w-1/2 h-40 p-2">
             <Link
               to="/page/all-in-one"
               className="flex justify-center items-center bg-turquois text-white text-center hover:bg-light-turquois w-full h-full font-roboto-mono text-base uppercase tracking-widest-2"
@@ -31,7 +31,7 @@ const Start: React.FC = () => {
               All In One
             </Link>
           </div>
-          <div className="flex w-1/2 h-64 p-2">
+          <div className="flex w-1/2 h-40 p-2">
             <Link
               to="/page/doubling-times"
               className="flex justify-center items-center bg-turquois text-white text-center hover:bg-light-turquois w-full h-full font-roboto-mono text-base uppercase tracking-widest-2"
@@ -39,7 +39,7 @@ const Start: React.FC = () => {
               Doubling Times
             </Link>
           </div>
-          <div className="flex w-full h-64 p-2">
+          <div className="flex w-full h-40 p-2">
             <Link
               to="/page/streamgraph"
               className="flex justify-center items-center bg-turquois text-white text-center hover:bg-light-turquois w-full h-full font-roboto-mono text-base uppercase tracking-widest-2"
